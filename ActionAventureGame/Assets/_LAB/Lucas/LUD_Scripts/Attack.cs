4ae3ad6817af1ec1e535b204f44ff4519a475508
Lucas <l.deutschmann@rubika-edu.com>
2020-02-18 18:24:44 +0100

    Test attaque

---
+++ ActionAventureGame/Assets/_LAB/Lucas/LUD_Scripts/Attack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject prefabHitbox;
+    [Range(0.0f, 10.0f)]
+    public float range;
+    //[Range(0.0f, 10.0f)]
+    //public float speed;
+    Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attaque();
+        }
+    }
+
+    void Attaque()
+    {
+        if (true)
+        {
+
+        }
+        Instantiate(prefabHitbox, transform.position + range *transform.right, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + 180));
+        Attaque_Movement();
+    }
+    void Attaque_Movement()
+    {
+        //transform.position += transform.right * Time.deltaTime * speed;
+    }
+}
