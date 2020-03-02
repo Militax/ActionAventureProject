fb0fc1a1e971e070f100ffce292055845d088d11
Lucas <l.deutschmann@rubika-edu.com>
2020-03-02 16:07:22 +0100

    attaque joueur

--- ActionAventureGame/Assets/_LAB/Lucas/LUD_Scripts/Attack.cs
+++ ActionAventureGame/Assets/_LAB/Lucas/LUD_Scripts/Attack.cs
@@ -2,49 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+
+/// <summary>
+/// Lucas Deutschmann
+/// 
+/// Script attaque joueur
+/// </summary>
+namespace Player
 {
-    public GameObject prefabHitbox;
-    [Range(0.0f, 10.0f)]
-    public float range;
-    public float cooldown;
-    bool isAttacking= false;
-    [Range(0.0f, 10.0f)]
-    public float speed;
-    Vector3 direction;
-    Vector3 mousePos;
-    // Start is called before the first frame update
-    void Start()
+    public class Attack : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Input.GetButtonDown("Fire1"))
+        public GameObject prefabHitbox;
+        [Range(0.0f, 10.0f)]
+        public float range;
+        public float cooldown;
+        bool isAttacking = false;
+        [Range(0.0f, 10.0f)]
+        public float speed;
+        Vector3 direction;
+        Vector3 attackPos;
+        float verticalDelta;
+        float horizontalDelta;
+        // Start is called before the first frame update
+        void Start()
         {
-            Attaque();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //attackPos = transform.position + Mathf.Sqrt(range) * transform.up + Mathf.Sqrt(range) * transform.right;
+            //horizontalDelta = Input.GetAxisRaw("Horizontal");
+            //verticalDelta = Input.GetAxisRaw("Vertical");
+
+            //if (verticalDelta > 0.2 || horizontalDelta > 0.2 || verticalDelta < -0.2 || horizontalDelta < -0.2)
+            //{
+            //    //verifie que le joystick est vraiment utilisé de manière volontaire
+            //    if (verticalDelta>=0 && horizontalDelta >=0)
+            //    {
+            //        // regarde en haut à droite
+            //        attackPos = transform.position + range * transform.up + range * transform.right;
+            //    }
+
+            //}
+
+
+
+
+
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Attaque();
+            }
+        }
+
+        void Attaque()
+        {
+
+            Instantiate(prefabHitbox, transform.position + range * transform.right, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + 180));
+            Attaque_Movement();
+            StartCoroutine(Attack_Cooldown());
+        }
+        void Attaque_Movement()
+        {
+            transform.position += (range * transform.right)/* * Time.deltaTime * speed*/;
+        }
+        IEnumerator Attack_Cooldown()
+        {
+            isAttacking = true;
+            yield return new WaitForSeconds(cooldown);
+            isAttacking = false;
         }
     }
 
-    void Attaque()
-    {
-        
-        Instantiate(prefabHitbox, transform.position + range *transform.right, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + 180));
-        Attaque_Movement();
-        StartCoroutine(Attack_Cooldown());
-    }
-    void Attaque_Movement()
-    {
-        transform.position += (range * transform.right)/* * Time.deltaTime * speed*/;
-    }
-    IEnumerator Attack_Cooldown()
-    {
-        isAttacking = true;
-        yield return new WaitForSeconds(cooldown);
-        isAttacking = false;
-    }
 }
