703db366e5038739769c1a82ba7937af03ae0c2f
Matis Duperray <m.duperray@rubika-edu.com>
2020-03-02 16:10:09 +0100

    Build test

---
+++ ActionAventureGame/Assets/Scripts/Powers/WindPower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Power
+{
+    public class WindPower : MonoBehaviour
+    {
+        public float duration = 1f;
+        public float power = 1f;
+
+        Rigidbody2D rb;
+        Vector2 WaveMovement;
+
+        void OnEnable()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            WaveMovement.x = 1;
+            WaveMovement.y = 0;
+            StartCoroutine(PowerDuration());
+        }
+
+        private void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + WaveMovement * power * Time.fixedDeltaTime);
+        }
+
+        IEnumerator PowerDuration()
+        {
+            yield return new WaitForSeconds(duration);
+            Destroy(gameObject);
+        }
+
+
+
+    }
+}
