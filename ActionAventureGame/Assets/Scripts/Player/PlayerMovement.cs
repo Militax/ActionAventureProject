76285b27637a1ec005d372e187d1753ce7048fc1
Lucas <l.deutschmann@rubika-edu.com>
2020-03-06 17:22:28 +0100

    fixed movement

--- ActionAventureGame/Assets/Scripts/Player/PlayerMovement.cs
+++ ActionAventureGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -12,8 +12,7 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         public float moveSpeed = 5f;
-        float fixedMoveSpeed;
-        float baseMoveSpeed;
+        
         Rigidbody2D rb;
         Vector3 movement;
 
@@ -23,8 +22,7 @@ namespace Player
         {
             //Recuperation du rigidbody du player
             rb = GetComponent<Rigidbody2D>();
-            fixedMoveSpeed = moveSpeed / Mathf.Sqrt(2);
-            baseMoveSpeed = moveSpeed;
+            
         }
         
         void Update()
@@ -36,18 +34,9 @@ namespace Player
 
         private void FixedUpdate()
         {
-            //On deplace le rigidbody en fonction des valeurs etablies
-            //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-            //rb.velocity = (transform.position - transform.position).normalized * moveSpeed;
-            if (movement.x != 0 && movement.y !=0)
-            {
-                moveSpeed = fixedMoveSpeed;
-            }
-            else
-            {
-                moveSpeed = baseMoveSpeed;
-            }
-            transform.position += movement * moveSpeed * Time.deltaTime;
+            
+            
+            transform.position += movement.normalized * moveSpeed * Time.deltaTime;
         }
     }
 }
