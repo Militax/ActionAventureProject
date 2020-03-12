4000d686bcd1311fe1d19909159305755865bd48
lucas <l.deutschmann@rubika-edu.com>
2020-03-12 22:53:58 +0100

    Ennemy Shoots

---
+++ ActionAventureGame/Assets/playerHP.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHP : MonoBehaviour
+{
+    public float BaseHP;
+    public float CurrentHP;
+    void Start()
+    {
+        CurrentHP = BaseHP;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            TakeDamage();
+        }
+    }
+    void TakeDamage()
+    {
+        CurrentHP -= 10;
+    }
+    private void Update()
+    {
+        if (CurrentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
