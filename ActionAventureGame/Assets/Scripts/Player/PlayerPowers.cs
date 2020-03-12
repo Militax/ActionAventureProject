7cee9ac08539d21de7202715fd04c40216f038ee
Matis Duperray <m.duperray@rubika-edu.com>
2020-03-12 15:32:39 +0100

    Pas mal de trucs sympa

--- ActionAventureGame/Assets/Scripts/Player/PlayerPowers.cs
+++ ActionAventureGame/Assets/Scripts/Player/PlayerPowers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Power;
+using GameManagement;
 
 
 /// <summary>
@@ -15,6 +16,8 @@ namespace Player
     {
         #region Variables
         public GameObject WindPowerPrefab;
+        public GameObject FrozenPowerPrefab;
+        public GameObject PowerPowerPrefab;
         #region Invocation Points
         public Transform topPoint;
         public Transform downPoint;
@@ -115,61 +118,221 @@ namespace Player
                 //Instantiation de la vague sur le bon point et dans la bonne direction en fonction du dernier angle enregistré par le joystick
                 switch (lookingAngle)
                 {
-                    case ("TOP"):
-                        GameObject WindWaveT = Instantiate(WindPowerPrefab, topPoint.position, topPoint.rotation);
 
-                        WindWaveT.GetComponent<WindPower>().WaveDirection.y = 1;
+
+                    //C'est la que y'as tous les commentaires
+                    case ("TOP"):
+
+                        GameObject WindWaveT;
+                        //Sélectionne le pouvoir en fonction du niveau du pouvoir
+                        switch(GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                //Instancie le pouvoir
+                                WindWaveT = Instantiate(WindPowerPrefab, topPoint.position, topPoint.rotation);
+                                //Donne la direction
+                                WindWaveT.GetComponent<WindPower>().WaveDirection.y = 1;
+                                break;
+                            case (2):
+                                WindWaveT = Instantiate(FrozenPowerPrefab, topPoint.position, topPoint.rotation);
+                                WindWaveT.GetComponent<WindPower>().WaveDirection.y = 1;
+                                break;
+                            case (3):
+                                WindWaveT = Instantiate(PowerPowerPrefab, topPoint.position, topPoint.rotation);
+                                WindWaveT.GetComponent<WindPower>().WaveDirection.y = 1;
+                                break;
+                        }
+                        
                         break;
+
+
+
 
                     case ("DOWN"):
-                        GameObject WindWaveD = Instantiate(WindPowerPrefab, downPoint.position, downPoint.rotation);
-
-                        WindWaveD.GetComponent<WindPower>().WaveDirection.y = (-1);
+                        GameObject WindWaveD;
+                        switch (GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                WindWaveD = Instantiate(WindPowerPrefab, downPoint.position, downPoint.rotation);
+                                WindWaveD.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                break;
+                            case (2):
+                                WindWaveD = Instantiate(FrozenPowerPrefab, downPoint.position, downPoint.rotation);
+                                WindWaveD.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                break;
+                            case (3):
+                                WindWaveD = Instantiate(PowerPowerPrefab, downPoint.position, downPoint.rotation);
+                                WindWaveD.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                break;
+                        }
+                        
                         break;
+
+
+
 
                     case ("LEFT"):
-                        GameObject WindWaveL = Instantiate(WindPowerPrefab, leftPoint.position, leftPoint.rotation);
+                        GameObject WindWaveL;
+                        switch (GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                WindWaveL = Instantiate(WindPowerPrefab, leftPoint.position, leftPoint.rotation);
+                                WindWaveL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                break;
+                            case (2):
+                                WindWaveL = Instantiate(FrozenPowerPrefab, leftPoint.position, leftPoint.rotation);
+                                WindWaveL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                break;
+                            case (3):
+                                WindWaveL = Instantiate(PowerPowerPrefab, leftPoint.position, leftPoint.rotation);
+                                WindWaveL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                break;
+                        }
 
-                        WindWaveL.GetComponent<WindPower>().WaveDirection.x = (-1);
                         break;
+
+
+
 
                     case ("RIGHT"):
-                        GameObject WindWaveR = Instantiate(WindPowerPrefab, rightPoint.position, rightPoint.rotation);
-
-                        WindWaveR.GetComponent<WindPower>().WaveDirection.x = 1;
+                        GameObject WindWaveR;
+                        switch (GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                WindWaveR = Instantiate(WindPowerPrefab, rightPoint.position, rightPoint.rotation);
+                                WindWaveR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                break;
+                            case (2):
+                                WindWaveR = Instantiate(FrozenPowerPrefab, rightPoint.position, rightPoint.rotation);
+                                WindWaveR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                break;
+                            case (3):
+                                WindWaveR = Instantiate(PowerPowerPrefab, rightPoint.position, rightPoint.rotation);
+                                WindWaveR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                break;
+                        }
+                        
                         break;
 
+
+
+                    //
                     case ("TOP LEFT"):
-                        GameObject WindWaveTL = Instantiate(WindPowerPrefab, topLeftPoint.position, topLeftPoint.rotation);
-
-                        WindWaveTL.GetComponent<WindPower>().WaveDirection.x = (-1);
-                        WindWaveTL.GetComponent<WindPower>().WaveDirection.y = 1;
-                        WindWaveTL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                        GameObject WindWaveTL;
+                        switch (GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                WindWaveTL = Instantiate(WindPowerPrefab, topLeftPoint.position, topLeftPoint.rotation);
+                                //Normalise la vitesse
+                                WindWaveTL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                WindWaveTL.GetComponent<WindPower>().WaveDirection.y = 1;
+                                WindWaveTL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (2):
+                                WindWaveTL = Instantiate(FrozenPowerPrefab, topLeftPoint.position, topLeftPoint.rotation);
+                                WindWaveTL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                WindWaveTL.GetComponent<WindPower>().WaveDirection.y = 1;
+                                WindWaveTL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (3):
+                                WindWaveTL = Instantiate(PowerPowerPrefab, topLeftPoint.position, topLeftPoint.rotation);
+                                WindWaveTL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                WindWaveTL.GetComponent<WindPower>().WaveDirection.y = 1;
+                                WindWaveTL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                        }
+                        
                         break;
+
+
+
 
                     case ("TOP RIGHT"):
-                        GameObject WindWaveTR = Instantiate(WindPowerPrefab, topRightPoint.position, topRightPoint.rotation);
+                        GameObject WindWaveTR;
+                        switch (GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                WindWaveTR = Instantiate(WindPowerPrefab, topRightPoint.position, topRightPoint.rotation);
+                                WindWaveTR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                WindWaveTR.GetComponent<WindPower>().WaveDirection.y = 1;
+                                WindWaveTR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (2):
+                                WindWaveTR = Instantiate(FrozenPowerPrefab, topRightPoint.position, topRightPoint.rotation);
+                                WindWaveTR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                WindWaveTR.GetComponent<WindPower>().WaveDirection.y = 1;
+                                WindWaveTR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (3):
+                                WindWaveTR = Instantiate(PowerPowerPrefab, topRightPoint.position, topRightPoint.rotation);
+                                WindWaveTR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                WindWaveTR.GetComponent<WindPower>().WaveDirection.y = 1;
+                                WindWaveTR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                        }
 
-                        WindWaveTR.GetComponent<WindPower>().WaveDirection.x = 1;
-                        WindWaveTR.GetComponent<WindPower>().WaveDirection.y = 1;
-                        WindWaveTR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
                         break;
+
+
+
 
                     case ("DOWN LEFT"):
-                        GameObject WindWaveDL = Instantiate(WindPowerPrefab, downLeftPoint.position, downLeftPoint.rotation);
+                        GameObject WindWaveDL;
+                        switch (GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                WindWaveDL = Instantiate(WindPowerPrefab, downLeftPoint.position, downLeftPoint.rotation);
+                                WindWaveDL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                WindWaveDL.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                WindWaveDL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (2):
+                                WindWaveDL = Instantiate(FrozenPowerPrefab, downLeftPoint.position, downLeftPoint.rotation);
+                                WindWaveDL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                WindWaveDL.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                WindWaveDL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (3):
+                                WindWaveDL = Instantiate(PowerPowerPrefab, downLeftPoint.position, downLeftPoint.rotation);
+                                WindWaveDL.GetComponent<WindPower>().WaveDirection.x = (-1);
+                                WindWaveDL.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                WindWaveDL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                        }
 
-                        WindWaveDL.GetComponent<WindPower>().WaveDirection.x = (-1);
-                        WindWaveDL.GetComponent<WindPower>().WaveDirection.y = (-1);
-                        WindWaveDL.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
                         break;
+
+
+
 
                     case ("DOWN RIGHT"):
-                        GameObject WindWaveDR = Instantiate(WindPowerPrefab, downRightPoint.position, downRightPoint.rotation);
+                        GameObject WindWaveDR;
+                        switch (GameManager.Instance.powerState)
+                        {
+                            case (1):
+                                WindWaveDR = Instantiate(WindPowerPrefab, downRightPoint.position, downRightPoint.rotation);
+                                WindWaveDR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                WindWaveDR.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                WindWaveDR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (2):
+                                WindWaveDR = Instantiate(FrozenPowerPrefab, downRightPoint.position, downRightPoint.rotation);
+                                WindWaveDR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                WindWaveDR.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                WindWaveDR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                            case (3):
+                                WindWaveDR = Instantiate(PowerPowerPrefab, downRightPoint.position, downRightPoint.rotation);
+                                WindWaveDR.GetComponent<WindPower>().WaveDirection.x = 1;
+                                WindWaveDR.GetComponent<WindPower>().WaveDirection.y = (-1);
+                                WindWaveDR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
+                                break;
+                        }
 
-                        WindWaveDR.GetComponent<WindPower>().WaveDirection.x = 1;
-                        WindWaveDR.GetComponent<WindPower>().WaveDirection.y = (-1);
-                        WindWaveDR.GetComponent<WindPower>().power /= Mathf.Sqrt(2);
                         break;
+
+
+
                 }
             }
         }
