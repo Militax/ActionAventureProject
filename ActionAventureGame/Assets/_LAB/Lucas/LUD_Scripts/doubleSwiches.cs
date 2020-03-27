f4565b09064987e18db155510b795fe329530837
lucas <l.deutschmann@rubika-edu.com>
2020-03-27 20:17:36 +0100

    boutons fonctionnels

---
+++ ActionAventureGame/Assets/_LAB/Lucas/LUD_Scripts/doubleSwiches.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doubleSwiches : MonoBehaviour
+{
+    bool isactive = false;
+    public doubleSwiches other;
+    private SpriteRenderer spr;
+    //public GameObject active;
+    //public GameObject inactive;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        isactive = (spr.color == Color.green);//ternaire  defini actiuf/ inactif selon la couleur du prefab
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (inactive.activeSelf)
+    //    {
+    //        isactive = false;
+    //    }
+    //}
+    public void Switch(bool state) 
+    {
+        if (state)
+        {
+            isactive = !state;
+            spr.color = (isactive ? Color.green : Color.red); // (Ternaire) si isactive = true : vert else :rouge
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Sword")
+        {
+            isactive = !isactive;
+            spr.color = (isactive ? Color.green : Color.red); // (Ternaire) si isactive = true : vert else :rouge
+            other.Switch(isactive);
+        }
+
+        //if (collision.tag == "Sword" && isactive == false)
+        //{
+        //    active.SetActive(true);
+        //    inactive.SetActive(false);
+        //    isactive = true;
+        //}
+        //else if (collision.tag == "Sword" && isactive == true)
+        //{
+        //    inactive.SetActive(true);
+        //    active.SetActive(false);
+        //    isactive = false;
+        //}
+    }
+}
---
+++ ActionAventureGame/Assets/pressureplatePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressureplatePlayer : MonoBehaviour
+{
+    bool isactive;
+    private SpriteRenderer spr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag =="Player")
+        {
+
+            spr.color = Color.yellow;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+
+            spr.color = Color.magenta;
+        }
+    }
+}
