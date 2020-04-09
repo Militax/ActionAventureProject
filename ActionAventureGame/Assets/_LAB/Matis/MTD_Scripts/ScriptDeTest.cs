3c3195b044f8f4f1d563591777df478aaedba706
Matis Duperray <m.duperray@rubika-edu.com>
2020-04-09 18:13:41 +0200

    Debut du bordel

---
+++ ActionAventureGame/Assets/_LAB/Matis/MTD_Scripts/ScriptDeTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+namespace Game
+{
+    
+
+    public class ScriptDeTest : MonoBehaviour
+    {
+        public List<Vector2> origineList;
+        private List<RayCheck> checkList;
+
+
+        private void Start()
+        {
+            checkList = new List<RayCheck>(); 
+
+            foreach (var item in origineList)
+            {
+                checkList.Add(new RayCheck(item));
+            }
+        }
+
+        private void Update()
+        {
+            foreach (var item in checkList)
+            {
+                if (item.isGood)
+                {
+                    var _ray = Physics2D.Raycast(transform.position, Vector2.right, 2);
+
+                    if (_ray.collider)
+                    {
+                        item.SetIsGood(false);
+                    }
+                }
+            }
+        }
+
+    }
+}
